8da8f6ca4c90d4bb7ee1dda23df92a3b16bf4d50
cjd6568358 <cjd6568358@163.com>
2019-04-25 15:10:19 +0800

    fix

---
+++ docs/precache-manifest.cbe862b9acf0601b15691f1bd4c5761e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "765f996baefc5f4ddd04",
+    "url": "/carpool/js/RecordSearchView.6845ac8b.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/carpool/robots.txt"
+  },
+  {
+    "revision": "d62b58ec31398f3b85b7",
+    "url": "/carpool/css/app.218b0b6b.css"
+  },
+  {
+    "revision": "9eaad9f540a07e0ebbde",
+    "url": "/carpool/css/HomeView~RecordInfoView.5f737518.css"
+  },
+  {
+    "revision": "ef47cb22f5b7fe01b504",
+    "url": "/carpool/css/MyView.fda57cf9.css"
+  },
+  {
+    "revision": "ef47cb22f5b7fe01b504",
+    "url": "/carpool/js/MyView.d6628643.js"
+  },
+  {
+    "revision": "c9fbfa5b66271203a1a5",
+    "url": "/carpool/css/RecordInfoView.0e433876.css"
+  },
+  {
+    "revision": "c9fbfa5b66271203a1a5",
+    "url": "/carpool/js/RecordInfoView.07b4b1c9.js"
+  },
+  {
+    "revision": "765f996baefc5f4ddd04",
+    "url": "/carpool/css/RecordSearchView.0f459118.css"
+  },
+  {
+    "revision": "04dbb41616433545d36e",
+    "url": "/carpool/js/HomeView.4900c707.js"
+  },
+  {
+    "revision": "9eaad9f540a07e0ebbde",
+    "url": "/carpool/js/HomeView~RecordInfoView.3fd84460.js"
+  },
+  {
+    "revision": "d62b58ec31398f3b85b7",
+    "url": "/carpool/js/app.5d945be4.js"
+  },
+  {
+    "revision": "bf78611ebcb3a5de49f5",
+    "url": "/carpool/js/betterScroll.2313a37e.js"
+  },
+  {
+    "revision": "82ff1c7c44ab83f725c8",
+    "url": "/carpool/js/chunk-vendors.f562346c.js"
+  },
+  {
+    "revision": "042c0f04eb71442eed77cb650feede30",
+    "url": "/carpool/img/logo.png"
+  },
+  {
+    "revision": "30f53f5ef5030d633fd899f9e0d1355d",
+    "url": "/carpool/index.html"
+  },
+  {
+    "revision": "042c0f04eb71442eed77cb650feede30",
+    "url": "/carpool/favicon.png"
+  },
+  {
+    "revision": "04dbb41616433545d36e",
+    "url": "/carpool/css/HomeView.b9b8209e.css"
+  },
+  {
+    "revision": "30f53f5ef5030d633fd899f9e0d1355d",
+    "url": "/carpool/404.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,7 +15,7 @@ importScripts("/carpool/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/carpool/workbox-v3.6.3"});
 
 importScripts(
-  "/carpool/precache-manifest.ae2b247bd9337ad75d30eecfc59323d2.js"
+  "/carpool/precache-manifest.cbe862b9acf0601b15691f1bd4c5761e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "carpool"});
